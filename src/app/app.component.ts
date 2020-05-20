67ba84b0098aef79d7e2cb85933127b0c928a369
portm4n <ruben_sanfer_3@hotmail.com>
2020-05-20 17:52:57 +0200

    mitad del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Capitan América';
+  arreglo =[1,2,3,4,5,6,7,8,9,10]
 }
